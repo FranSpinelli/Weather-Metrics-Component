2a7a1e2899e6cc0d22dcaeaeff54fd4072cd5eb6
franciscos <franciscos@pagotic.com>
2025-06-09 21:41:53 -0300

    Proyect Init

---
+++ src/main/java/ar/edu/unq/weather_metrics_component/WeatherMetricsComponentApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.weather_metrics_component;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMetricsComponentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherMetricsComponentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/edu/unq/weather_metrics_component/WeatherMetricsComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.weather_metrics_component;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherMetricsComponentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
